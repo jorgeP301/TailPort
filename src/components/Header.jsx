d5f52c4fa6fe24f0308dcde8b13c83bf219d472a
Jorge Patino <Jmp17450@uga.edu>
2025-11-03 23:28:09 -0500

    remove unnecessary files and make some small changes
    more structure and less unneeded files

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,24 @@ import React from "react";
 
 function Header() {
     return (
-        <nav className="bg-blue-shade px-4 py-2 flex flex-1 rounded-xl">
-            <button className="p-2">My Portfolio</button>    
-            <button className="p-2">About Me</button>
-            <button className="p-2">Projects</button>
-            <button className="p-2">Contact</button>
+        <nav className="bg-blue-shade-800 px-4 py-2 rounded-xl">
+            <div className="flex justify-around">
+                <div className="flex rounded-xl hover:bg-blue-shade-600">
+                    <a className="p-2 ">My Portfolio</a>
+                </div>
+                <div className="flex rounded-xl hover:bg-blue-shade-600">
+                    <a className="p-2">About Me</a>
+                </div>
+                <div className="flex rounded-xl hover:bg-blue-shade-600" onClick={console.log("hello")}>
+                    <a className="p-2">Projects</a>
+                </div>
+                <div className="flex rounded-xl hover:bg-blue-shade-600">
+                    <a className="p-2">Contact</a>
+                </div>
+
+
+
+            </div>
         </nav>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import About from './pages/About.jsx'
 import Header from './components/Header.jsx'
 import Projects from './pages/Projects.jsx'
