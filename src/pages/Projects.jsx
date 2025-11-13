d291ca9d83712bd4303051b2b16a257ab2772285
Jorge Patino <Jmp17450@uga.edu>
2025-11-13 16:57:03 -0500

    add gap

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,7 +5,7 @@ import Carousel from "../components/Carousel.jsx";
 function Projects() {
 
     return (
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center gap-y-6">
             <h1 className="font-jost h-auto text-4xl text-center mt-10 text-white">Projects Page</h1>
             <Carousel />
         </div>
